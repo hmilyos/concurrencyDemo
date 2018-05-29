086cb71e77b76d7f317eff434b08f5cf8212736d
欧世明 <ousm@minstone.com.cn>
2018-05-29 08:42:22 +0800

    init

---
+++ src/main/java/com/hmily/concurrencyDemo/ConcurrencyDemoApplication.java
@@ -0,0 +1,12 @@
+package com.hmily.concurrencyDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcurrencyDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConcurrencyDemoApplication.class, args);
+	}
+}
